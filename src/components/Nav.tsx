ac0e5b373dcddbf3e1121b47a526df4f54855b3a
Tyrese-FullStackGenius <tyresejin3915@gmail.com>
2023-01-16 03:58:20 -0400

    Fix exports / imports

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
-import { Logo } from '@/static';
+import { Logo } from '@static';
 export const Nav = () => {
   return (
     <nav>
