dde689e9f9ffe78ae71e300d48fab813a7c7b74c
Tyrese-FullStackGenius <tyresejin3915@gmail.com>
2023-01-18 02:17:50 -0400

    New
    - CTA Component

---
+++ src/shared/ActionButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { SelectedLink } from '@/utils/types';
+
+type Props = {
+  children: React.ReactNode;
+  setSelectedLink: (value: SelectedLink) => void;
+};
+
+export const ActionButton = ({ children, setSelectedLink }: Props) => {
+  return (
+    <AnchorLink
+      className="rounded-md bg-neutral-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
+      onClick={() => setSelectedLink(SelectedLink.ContactUs)}
+      href={`${SelectedLink.ContactUs}`}
+    >
+      {children}
+    </AnchorLink>
+  );
+};
