427f8f6605b0249c6c68194d43e62304ed1194ed
Tyrese-FullStackGenius <tyresejin3915@gmail.com>
2023-01-18 02:18:30 -0400

    New
    - Completed Navbar functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Nav } from '@/components';
 import { SelectedLink } from '@/utils/types';
 
 export const App = () => {
   const [selectedLink, setSelectedLink] = useState<SelectedLink>(SelectedLink.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY === 0 ? (setIsTopOfPage(true), setSelectedLink(SelectedLink.Home)) : setIsTopOfPage(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <div className="app bg-gray-20">
-      <Nav selectedLink={selectedLink} setSelectedLink={setSelectedLink} />
+      <Nav isTopOfPage={isTopOfPage} selectedLink={selectedLink} setSelectedLink={setSelectedLink} />
       <main>
         <div></div>
       </main>
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,20 +5,22 @@ import { flexBetween } from '@/utils/styles';
 import Links from './Links';
 import { SelectedLink } from '@/utils/types';
 import { useMQ } from '@/hooks/useMQ';
-
+import { ActionButton } from '@/shared/ActionButton';
 interface Props {
   selectedLink: SelectedLink;
   setSelectedLink: (value: SelectedLink) => void;
+  isTopOfPage: boolean;
 }
 
-export const Nav = ({ selectedLink, setSelectedLink }: Props) => {
+export const Nav = ({ selectedLink, setSelectedLink, isTopOfPage }: Props) => {
   const isDesktop = useMQ('(min-width: 1060px)');
   const [toggle, setToggle] = useState(false);
+  const navBarBg = !isTopOfPage && 'bg-primary-100 drop-shadow';
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${navBarBg} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
-          <div className={`${flexBetween} w-full gap-16`}>
+          <div className={`${flexBetween} w-full gap-16 font-bold`}>
             <img src={Logo} alt="EgoGym" />
             {isDesktop ? (
               <div className={`${flexBetween} w-full`}>
@@ -30,17 +32,34 @@ export const Nav = ({ selectedLink, setSelectedLink }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign in</p>
-                  <button>Become a Member</button>
+                  <ActionButton setSelectedLink={setSelectedLink}>Become a Member</ActionButton>
                 </div>
               </div>
             ) : (
               <button className="rounded-full bg-neutral-500 p-2" onClick={() => setToggle(!toggle)}>
-                {!toggle ? <Bars3Icon className="h-6 w-6 text-white" /> : <XMarkIcon className="h-6 w-6 text-white" />}
+                <Bars3Icon className="aspect-square w-6 text-white" />
               </button>
             )}
           </div>
         </div>
       </div>
+
+      {!isDesktop && toggle && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button onClick={() => setToggle(!toggle)}>
+              <XMarkIcon className="aspect-square w-6 text-gray-400 " />
+            </button>
+          </div>
+
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Links link="Home" selectedLink={selectedLink} setSelectedLink={setSelectedLink} />
+            <Links link="Benefits" selectedLink={selectedLink} setSelectedLink={setSelectedLink} />
+            <Links link="Our Classes" selectedLink={selectedLink} setSelectedLink={setSelectedLink} />
+            <Links link="Contact Us" selectedLink={selectedLink} setSelectedLink={setSelectedLink} />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
