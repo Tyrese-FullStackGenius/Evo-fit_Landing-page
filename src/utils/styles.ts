7157f7cd6cd90242c9767d51b7b8725754fb884f
Tyrese-FullStackGenius <tyresejin3915@gmail.com>
2023-01-19 02:09:06 -0400

    New
    - Create overlayStyles

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,1 +1,5 @@
 export const flexBetween = 'flex items-center justify-between';
+export const overlayStyles = `p-5 absolute z-30 flex
+h-[380px] w-[450px] flex-col items-center justify-center
+whitespace-normal bg-primary-500 text-center text-white
+opacity-0 transition duration-500 hover:opacity-90`;
